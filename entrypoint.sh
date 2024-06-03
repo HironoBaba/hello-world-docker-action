f0718b054803adbc67def9201840acde3d71ef20
HironoBaba <88128407+HironoBaba@users.noreply.github.com>
2024-06-03 21:24:29 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 # Use INPUT_<INPUT_NAME> to get the value of an input
-GREETING="Hello, $INPUT_WHO_TO_GREET!"
+#　GREETING="Hello, $INPUT_WHO_TO_GREET!"
 
 # Use workflow commands to do things like set debug messages
 echo "::notice file=entrypoint.sh,line=7::$GREETING"
@@ -9,4 +9,12 @@ echo "::notice file=entrypoint.sh,line=7::$GREETING"
 # Write outputs to the $GITHUB_OUTPUT file
 echo "time=$(date)" >>"$GITHUB_OUTPUT"
 
-exit 0
+#exit 0
+
+FROM alpine:3.10
+
+COPY entrypoint.sh /entrypoint.sh
+
+RUN chmod +x entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
